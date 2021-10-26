ec1be0bbc475bfff91fd7c3b56f0ab6fcc969d26
warmrome <pxpx786@gmail.com>
2021-10-26 10:02:38 -0700

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Jameson Hurst on 10/26/21.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
