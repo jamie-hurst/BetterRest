53f7262b988540c8ea396a3ee776ba824e8f3e70
warmrome <pxpx786@gmail.com>
2021-10-28 12:17:52 -0700

    Tweaked layout/heading text to space out elements.  Removed the button to calculate logic and implemented this as a computed property instead. The result will always be showing at the bottom.

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -21,67 +21,8 @@ struct ContentView: View {
     @State private var sleepAmount = 8.0
     @State private var coffeeAmount = 1
     
-    @State private var alertTitle = ""
-    @State private var alertMessage = ""
-    @State private var showingAlert = false
     
-    
-    var body: some View {
-        
-        NavigationView {
-            Form {
-                
-                Section {
-                    VStack(alignment: .leading, spacing: 10) {
-                        Text("When do you want to wake up?")
-                            .font(.headline)
-                        
-                        DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
-                            .labelsHidden()
-                    }
-                }
-                
-                
-                
-                Section {
-                    VStack(alignment: .leading, spacing: 10) {
-                        Text("Desired amount of sleep")
-                            .font(.headline)
-                        
-                        Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
-                    }
-                }
-                
-                
-                
-                Section {
-                    VStack(alignment: .leading, spacing: 10) {
-                        Text("Daily coffee intake")
-                            .font(.headline)
-                        
-                        Stepper(coffeeAmount == 1 ? "1 cup" : "\(coffeeAmount) cups", value: $coffeeAmount, in: 1...20)
-                    }
-                }
-                
-                
-            }
-            .navigationTitle("BetterRest")
-            .toolbar {
-                Button("Calculate", action: calculateBedtime)
-                    .alert(alertTitle, isPresented: $showingAlert) {
-                        Button("OK") { }
-                    } message: {
-                        Text(alertMessage)
-                    }
-            }
-            
-            
-        }
-        
-    }
-    
-    
-    func calculateBedtime() {
+    var sleepResults: String {
         do {
             let config = MLModelConfiguration()
             let model = try SleepCalculator(configuration: config)
@@ -93,16 +34,61 @@ struct ContentView: View {
             let prediction = try model.prediction(wake: Double(hour + minute), estimatedSleep: sleepAmount, coffee: Double(coffeeAmount))
             
             let sleepTime = wakeUp - prediction.actualSleep
-            alertTitle = "Your ideal bedtime is…"
-            alertMessage = sleepTime.formatted(date: .omitted, time: .shortened)
+            
+            return sleepTime.formatted(date: .omitted, time: .shortened)
+            
         } catch {
-            alertTitle = "Error"
-            alertMessage = "Sorry, there was a problem calculating your bedtime."
+            return "There was an error."
         }
-        
-        showingAlert = true
     }
     
+    
+    var body: some View {
+        
+        NavigationView {
+            Form {
+                
+                Section {
+                    DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                        .labelsHidden()
+                } header: {
+                    Text("When do you want to wake up?")
+                        .textCase(.none)
+                        .font(.subheadline)
+                }
+                
+                
+                Section {
+                    Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+                } header: {
+                    Text("Desired amount of sleep")
+                        .textCase(.none)
+                        .font(.subheadline)
+                }
+                
+                
+                Section {
+                    Stepper(coffeeAmount == 1 ? "1 cup" : "\(coffeeAmount) cups", value: $coffeeAmount, in: 1...20)
+                } header: {
+                    Text("Daily coffee intake")
+                        .textCase(.none)
+                        .font(.subheadline)
+                }
+                
+                
+                Section {
+                    Text(sleepResults)
+                } header: {
+                    Text("Recommended wake time")
+                        .textCase(.none)
+                        .font(.subheadline)
+                }
+                
+            }
+            .navigationTitle("BetterRest")
+            
+        }
+    }
 }
 
 struct ContentView_Previews: PreviewProvider {
